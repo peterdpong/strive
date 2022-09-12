1c8f1b503bee741115a0a77859b838aee262aad5
Peter Damrongpiriyapong <peterdpong@gmail.com>
2022-09-12 15:47:48 -0400

    Add filler onboarding index page

---
+++ pages/onboarding/index.tsx
@@ -0,0 +1,14 @@
+import { Box, Button, Center, Container, Heading, Text } from "@chakra-ui/react";
+
+
+export default function OnboardingIndex() {
+  return (
+    <Container bg={'gray.300'} maxW='container.md' rounded={'5px'} my={'25px'} p={'25px'} centerContent>
+
+        <Heading textAlign={'center'}>Welcome to financial-optimizer.</Heading>
+        <Text fontSize={'xl'}>Let&apos;s build your financial plan.</Text>
+        <Button>Get Started.</Button>
+
+    </Container>
+  )
+}
