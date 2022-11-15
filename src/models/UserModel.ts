667b21b4416ffe12c668769792f9083353e020c5
Peter Damrongpiriyapong <peterdpong@gmail.com>
2022-11-15 17:43:54 -0500

    Add new models to UserModel

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -1,11 +1,17 @@
+import { Transaction } from "./BudgetModel";
+import { GoalModel } from "./GoalModel";
+
 export type UserModel = {
   uid: string;
   email: string;
   firstName: string;
   lastName: string;
+  financialInfo: UserFinancialInfo;
+  goalInfo: GoalModel;
+  monthTransactionsMap: { [key: string]: Transaction[] };
 };
 
-export type UserFinancials = {
+export type UserFinancialInfo = {
   income: number;
   fixedCosts: number;
   accounts: Account[];
@@ -23,18 +29,19 @@ export type Account = {
   type: AccountType;
 };
 
-export const createUserModel = (
-  uid: string,
-  email: string,
-  firstName: string,
-  lastName: string
-) => {
-  const newUserModel: UserModel = {
-    uid,
-    email,
-    firstName,
-    lastName,
-  };
+// TODO(Peter): Clean up once sure we do no need this.
+// export const createUserModel = (
+//   uid: string,
+//   email: string,
+//   firstName: string,
+//   lastName: string
+// ) => {
+//   const newUserModel: UserModel = {
+//     uid,
+//     email,
+//     firstName,
+//     lastName,
+//   };
 
-  return newUserModel;
-};
+//   return newUserModel;
+// };
