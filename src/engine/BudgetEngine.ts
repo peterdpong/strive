20e4ca112dae875b11b1bda04d40772495a58e81
Joel Lotzkar <joel.lotzkar@gmail.com>
2023-01-24 21:04:45 -0500

    present net worth calc

--- src/engine/BudgetEngine.ts
+++ src/engine/BudgetEngine.ts
@@ -1,8 +1,28 @@
 // Holds all budget optimization functions
 
+import { UserModel } from "../models/UserModel";
+
 export class BudgetEngine {
   static generateGoals(userData: UserModel, goalNetWorth: number) {
+    //formula 1: net worth differential
+
     //present net worth
+
+    //total assets
+    const totalAssets: number =
+      userData.financialInfo.accounts.bankAccounts.BankInvestmentAccount.value +
+      userData.financialInfo.accounts.fixedInvestments.FixedInvestment
+        .startingValue +
+      userData.financialInfo.accounts.otherAssets.OtherAsset.value;
+
+    //total liabilities
+    const totalLiabilities: number =
+      userData.financialInfo.accounts.loans.LoanAccount.remainingAmount +
+      userData.financialInfo.accounts.creditCards.CreditCardAccount.amountOwned;
+
+    //present net worth
+    const currNetWorth = totalAssets - totalLiabilities;
+
     //goal net worth
 
     userData.return;
