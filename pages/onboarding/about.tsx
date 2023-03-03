52022cc65b74788b7b9871cd97410359b4a211bd
Peter Damrongpiriyapong <peterdpong@gmail.com>
2023-03-03 11:12:43 -0500

    Fix linter build issue

--- pages/onboarding/about.tsx
+++ pages/onboarding/about.tsx
@@ -1,36 +1,29 @@
-import {
-    Button,
-    Container,
-    Heading,
-    Text,
-    Flex,
-    Spacer,
-    VStack
-  } from "@chakra-ui/react";
+import { Button, Container, Heading, Text } from "@chakra-ui/react";
 
 import { useRouter } from "next/router";
 import ProtectedRoute from "../../src/auth/ProtectedRoute";
 
 export default function AboutPage() {
-    const router = useRouter();
+  const router = useRouter();
 
-return (
+  return (
     <ProtectedRoute>
-        <Container maxW="container.xl" my={"25px"}>
-            <Button size="sm" onClick={() => router.push("/onboarding/")}>
-                Back
-            </Button>
-            <Heading>About Strive</Heading>
-            <Text fontSize={"md"}>Strive is your personal finance tool for budgeting.
-            Input your finances and target net worth and receive personalized suggestions
-            on how to best reach your goal.  Suggestions are supported by calculations and
-            sources where appropriate.  Disclaimer: the outputs are suggestions and should not be
-            taken as financial advice.
-            </Text>
-                <Button size="sm" onClick={() => router.push("/onboarding/finances/")}>
-                    Get Started
-                </Button>      
-        </Container>
+      <Container maxW="container.xl" my={"25px"}>
+        <Button size="sm" onClick={() => router.push("/onboarding/")}>
+          Back
+        </Button>
+        <Heading>About Strive</Heading>
+        <Text fontSize={"md"}>
+          Strive is your personal finance tool for budgeting. Input your
+          finances and target net worth and receive personalized suggestions on
+          how to best reach your goal. Suggestions are supported by calculations
+          and sources where appropriate. Disclaimer: the outputs are suggestions
+          and should not be taken as financial advice.
+        </Text>
+        <Button size="sm" onClick={() => router.push("/onboarding/finances/")}>
+          Get Started
+        </Button>
+      </Container>
     </ProtectedRoute>
-);
+  );
 }
