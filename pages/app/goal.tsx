bd298f4da338e5a55e54f2d6f15eaeef3bc26ccd
song <songeun.you7@gmail.com>
2023-03-12 17:23:35 -0400

    Add logic to calculate net worth over time

--- pages/app/goal.tsx
+++ pages/app/goal.tsx
@@ -37,19 +37,47 @@ ChartJS.register(
 export default function GoalPage() {
   const { useRequiredAuth } = useAuth();
   const userData = useRequiredAuth();
+  if (userData === null) {
+    return null;
+  }
 
+  // graph for net worth goal
   let graphData = buildGoalGraphData({
     userData: userData,
     monthlySavings: userData?.goalInfo.monthlyAmount,
     goalTimeline: userData?.goalInfo.timelineGoal,
   });
 
+  // starting value for current net worth
   let currNetWorth = calculateNetWorth(userData);
 
+  // add up all the transactions for each month
+  let netMonthlySpending = Object.keys(userData.monthTransactionsMap).map(
+    (month) => {
+      return userData.monthTransactionsMap[month].reduce(
+        (netSpending, newTransaction) => {
+          return (netSpending += newTransaction.amount);
+        },
+        0
+      );
+    }
+  );
+
+  // construct the array of how net worth changes month-to-month
+  // only accounts for unallocated income + transactions
+  // no investments are factored into this calculation yet
+  let currSpending = currNetWorth;
+  let netWorthOverTime = netMonthlySpending.map((spending) => {
+    currSpending +=
+      spending + userData.budgetInfo.monthlyVariableBudgetUnallocated;
+    return currSpending;
+  });
+
+  // add current tracked net worth to the graph
   graphData?.datasets.unshift({
     fill: true,
     label: "Current Net Worth",
-    data: [currNetWorth],
+    data: netWorthOverTime,
     borderColor: "rgb(60, 20, 240)",
     backgroundColor: (context: ScriptableContext<"line">) => {
       const ctx = context.chart.ctx;
